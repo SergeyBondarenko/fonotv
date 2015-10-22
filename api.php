fb5d904901e4db6c1b5b86970e6f1202e98972d3
Sergey Bonadrenko <sergibondarenko@gmail.com>
2015-10-22 12:34:23 -0500

    Add url validator for api.php

--- api.php
+++ api.php
@@ -5,21 +5,28 @@ include 'php/functions.php';
 $pageUrl = $_POST['url'];
 $dbFile = 'json/coubs.json';
 
-$html = remote_get_contents($pageUrl);
+//Verify URL
+$pageUrl = filter_var($pageUrl, FILTER_SANITIZE_URL);
 
-$pageUrl = get_coub_video_link($html, $pageUrl);
-
-if(file_exists($dbFile)){
-	$videoJson = file_get_contents($dbFile, LOCK_EX);
-	$videoArray = json_decode($videoJson, true);
-	$newEid = count($videoArray);
-} else {
-	$newEid = 0;
-} 
-
-$videoArray[$newEid]["file"] = $pageUrl;
-
-file_put_contents($dbFile, json_encode($videoArray), LOCK_EX);
-//file_put_contents($dbFile, $pageUrl.PHP_EOL, FILE_APPEND | LOCK_EX);
-
+if (!filter_var($pageUrl, FILTER_VALIDATE_URL) === false) {
+  
+  //Get HTML for coub.com
+  $html = remote_get_contents($pageUrl);
+  
+  //Parse HTML and get url for coub video
+  $pageUrl = get_coub_video_link($html, $pageUrl);
+  
+  if(file_exists($dbFile)){
+  	$videoJson = file_get_contents($dbFile, LOCK_EX);
+  	$videoArray = json_decode($videoJson, true);
+  	$newEid = count($videoArray);
+  } else {
+  	$newEid = 0;
+  } 
+  
+  $videoArray[$newEid]["file"] = $pageUrl;
+  
+  file_put_contents($dbFile, json_encode($videoArray), LOCK_EX);
+  //file_put_contents($dbFile, $pageUrl.PHP_EOL, FILE_APPEND | LOCK_EX);
+}
 ?>

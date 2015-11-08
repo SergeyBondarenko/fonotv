6359792b90c0f986e6e996da5a81b04c2ab5b151
Sergey Bondarenko <sergibondarenko@gmail.com>
2015-11-08 20:21:17 +0000

    Clean old code.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -8,20 +8,11 @@ function playVideo(video_source, videoTag){
 	var videoCount = video_source.length; 
 	var videoId = 0;
 	
-  //var local_file = video_source[0].file;
-  //local_file = local_file.split("/");
-  //local_file = "video/" + local_file[local_file.length - 1];
-	//document.getElementById(videoTag).setAttribute("src",local_file);
 	document.getElementById(videoTag).setAttribute("src",video_source[0].file);
-	 
 	document.getElementById(videoTag).addEventListener('ended',myHandler,false);
 
 	function videoPlay(videoNum)
 	{
-    //local_file = video_source[videoNum].file;
-    //local_file = local_file.split("/");
-    //local_file = "video/" + local_file[local_file.length - 1];
-	  //document.getElementById(videoTag).setAttribute("src",video_source[videoNum].file);
 	  document.getElementById(videoTag).setAttribute("src",video_source[videoNum].file);
 	  document.getElementById(videoTag).load();
 	  document.getElementById(videoTag).play();
@@ -42,38 +33,13 @@ var video_source = {};
 app.controller('MainController', ['$scope', 'coub', function($scope, coub){
 	
 	$scope.coubText = 'Space ship';
-	//var search_coubs = 'php/searchcoubs.php';
-	//var download_coubs = 'php/downloadcoubs.php';
 	var local_json = 'php/apimongo.php';
-	//var system_calls = 'php/systemcalls.php';
-
-	// Default play video
-	//coub.getCoubs(search_coubs, $scope.coubText).success(function(data){
-	//	video_source = data.coubs;	
-	//	playVideo(video_source, "myVideo");
-	//});
 
 	// Default play FonoTV video
 	coub.getCoubsForFTV(local_json).success(function(data){
 		video_source = data;	
 		playVideo(video_source, "video-about");
 	});
-
-	// Search for video and insert it
-  //$scope.fetch = function(){
-	//	// Search for video
-  //  coub.getCoubs(search_coubs, $scope.coubText).success(function(data){
-  //    video_source = data.coubs;
-	//		// Insert video in DOM
-	//		playVideo(video_source, "myVideo");
-  //  }); 
-  //}
-
-	//$scope.downloadCoub = function(){
-	//	coub.getCoubs(download_coubs, $scope.coubPageUrl).success(function(data){
-	//		$scope.coubVideoLink = data;
-	//	});
-	//}
 
 	$scope.readDbFTV = function(){
 		// Play FonoTV video
--- js/services/coub.js
+++ js/services/coub.js
@@ -2,17 +2,6 @@ app.factory('coub', ['$http', function($http){
 
 	var coub = {};
 
-	//coub.getCoubs = function(phpScript, coubText){
-	//	var urlBase = phpScript;
-  //  return $http.post(urlBase, {coubText: coubText})
-  //  .success(function(data){
-  //    return data;
-  //  })
-  //  .error(function(err){
-  //    return err;
-  //  });
-	//};
-	
 	coub.getCoubsForFTV = function(local_json){
     return $http.post(local_json)
     .success(function(data){

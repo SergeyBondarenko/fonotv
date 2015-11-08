c82af8e73cceddeff6c68ecb770172f4c83d5cb3
Sergey Bondarenko <sergibondarenko@gmail.com>
2015-11-08 21:13:04 +0000

    Put mute handeling in controller.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -13,6 +13,7 @@ function playVideo(video_source, videoTag){
 	
 	video.setAttribute("src",video_source[0].file);
 	video.addEventListener('ended',myHandler,false);
+
 	play_music.addEventListener('click', function(){
   	muteVideo(video); 
   }, false);
@@ -24,20 +25,31 @@ function playVideo(video_source, videoTag){
 			video.muted = true;	
 	}
 
-	function videoPlay(videoNum)
+	function videoPlay(videoNum, mute)
 	{
 	  video.setAttribute("src",video_source[videoNum].file);
 	  video.load();
+		if(mute == true)
+			video.muted = true;	
+		else
+			video.muted = false;
+
 	  video.play();
 	}
 
 	function myHandler(){
+		var mute = "";
+		if(video.muted)
+			mute = true;
+		else
+			mute = false;
+
 	  if (videoId == (videoCount - 1)){
 	    videoId = 0;
-	    videoPlay(videoId);
+	    videoPlay(videoId, mute);
 	  } else {
 	    videoId++;
-	    videoPlay(videoId);
+	    videoPlay(videoId, mute);
 	  }
 	}
 }

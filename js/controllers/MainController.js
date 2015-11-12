34e1321a6fe2da18433357c5fca84a9d3370e301
Sergey Bondarenko <sergibondarenko@gmail.com>
2015-11-12 22:10:17 +0000

    Experimenting with links to orig page.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,6 +1,8 @@
 function playVideo(video_source, videoTag){
 	var video = document.getElementById(videoTag); 
 	var play_music = document.getElementById('play-music');
+	var video_link = document.getElementById('orig-link');
+	var video_link_title = document.getElementById('orig-link-title');
 
 	// Test console output
 	for(var i = 0; i < video_source.length; i++){
@@ -11,13 +13,19 @@ function playVideo(video_source, videoTag){
 	var videoCount = video_source.length; 
 	var videoId = 0;
 	
+	// Set first video
 	video.setAttribute("src",video_source[0].file);
+	//video_link.setAttribute("href",video_source[0].orig_page);
+	//video_link_title.textContent = video_source[0].title;
+
+	// Listen for video end and run handler to play other videos
 	video.addEventListener('ended',myHandler,false);
 
 	play_music.addEventListener('click', function(){
   	muteVideo(video); 
   }, false);
 
+	// Mute and unmute
 	function muteVideo(video){
 		if(video.muted)
 			video.muted = false;
@@ -25,9 +33,13 @@ function playVideo(video_source, videoTag){
 			video.muted = true;	
 	}
 
+	// Play all other videos
 	function videoPlay(videoNum, mute)
 	{
 	  video.setAttribute("src",video_source[videoNum].file);
+		//video_link.setAttribute("href",video_source[videoNum].orig_page);
+		//video_link_title.textContent = video_source[videoNum].title;
+
 	  video.load();
 		if(mute == true)
 			video.muted = true;	
--- php/apimongo.php
+++ php/apimongo.php
@@ -11,6 +11,17 @@ $docs_array = array();
 
 foreach($docs as $doc){
 	$docs_array[] = array("file" => $doc["file"]);
+	//$docs_array[] = array("orig_page" => $doc["orig_page"]);
+	//$docs_array[] = array("title" => $doc["title"]);
+	//$docs_array["source"] = $doc["source"];
+	//$docs_array["orig_page"] = $doc["orig_page"];
+	//$docs_array["likes"] = $doc["likes"];
+	//$docs_array["file"] = $doc["file"];
+	//$docs_array["video_mobile_res"] = $doc["video_mobile_res"];
+  //$docs_array["video_high_res_muted"] = $doc["video_high_res_muted"];
+  //$docs_array["audio_high_res"] = $doc["audio_high_res"];
+  //$docs_array["video_med_res_muted"] = $doc["video_med_res_muted"];
+  //$docs_array["audio_med_res"] = $doc["audio_med_res"];
 }
 
 echo json_encode($docs_array);

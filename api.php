3ab4bdad2567779496564a7c6229f919c0902b48
SergeyBondarenko <sergibondarenko@gmail.com>
2015-11-12 16:55:41 +0100

    Add comments

--- api.php
+++ api.php
@@ -14,6 +14,7 @@ if (!filter_var($pageUrl, FILTER_VALIDATE_URL) === false) {
   $my_path = parse_url($pageUrl, PHP_URL_PATH);
   $my_path_arr = explode("/", $my_path);
 
+//Verify if url is from coub.com
   if(($my_host == "coub.com") && ($my_path_arr[1] == "view") && (sizeof($my_path_arr) == 3)){
   
     //Get HTML for coub.com
@@ -22,26 +23,15 @@ if (!filter_var($pageUrl, FILTER_VALIDATE_URL) === false) {
     //Parse HTML and get url for coub video
     $pageUrl = get_coub_video_link($html, $pageUrl);
     
-	  // Save to db
+	  // Connect to DB
 	  $mongo = new MongoClient();
 	  $db = $mongo->fonotv;
 	  $collection = $db->hyperlinks;
 	  
+	  // Save to DB
 	  $document = array("file" => $pageUrl);
 	  $collection->insert($document);
   }
 
-  //if(file_exists($dbFile)){
-  //	$videoJson = file_get_contents($dbFile, LOCK_EX);
-  //	$videoArray = json_decode($videoJson, true);
-  //	$newEid = count($videoArray);
-  //} else {
-  //	$newEid = 0;
-  //} 
-  //
-  //$videoArray[$newEid]["file"] = $pageUrl;
-  //
-  //file_put_contents($dbFile, json_encode($videoArray), LOCK_EX);
-  ////file_put_contents($dbFile, $pageUrl.PHP_EOL, FILE_APPEND | LOCK_EX);
 }
 ?>
